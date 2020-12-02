829761fdd9933b9ab048fcaef8c567742b9923eb
Matt Beveridge <mattbev@mit.edu>
2020-12-01 23:34:16 -0500

    refined command_iiwa_position function, and new implementation of JugglerStation

--- juggler.py
+++ juggler.py
@@ -1,5 +1,6 @@
 import sys
 import numpy as np
+import pandas as pd
 
 # Install pyngrok.
 server_args = []
@@ -9,48 +10,71 @@ from meshcat.servers.zmqserver import start_zmq_server_as_subprocess
 proc, zmq_url, web_url = start_zmq_server_as_subprocess(server_args=server_args)
 
 from pydrake.all import (
-    DiagramBuilder, ConnectMeshcatVisualizer, Simulator
+    DiagramBuilder, ConnectMeshcatVisualizer, Simulator, SignalLogger
 )
 
-from utils.station import make_manipulation_station
+from utils.station import JugglerStation
 
 class Juggler:
-    def __init__(self, time_step=0.002):
+    def __init__(self, kp=100, ki=1, kd=20, time_step=0.002):
         """
-        Robotic Kuka IIWA juggler with paddle end effector
+        Robotic Kuka IIWA juggler with paddle end effector     
 
         Args:
+            kp (int, optional): proportional gain. Defaults to 100.
+            ki (int, optional): integral gain. Defaults to 1.
+            kd (int, optional): derivative gain. Defaults to 20.
             time_step (float, optional): time step for internal manipulation station controller. Defaults to 0.002.
         """        
         self.time = 0
+        juggler_station = JugglerStation(
+            kp=kp,
+            ki=ki,
+            kd=kd,
+            time_step=time_step)
+
         self.builder = DiagramBuilder()
-        self.station = self.builder.AddSystem(make_manipulation_station(time_step))
+        self.station = self.builder.AddSystem(juggler_station.get_diagram())
+        self.log = []
+
         self.visualizer = ConnectMeshcatVisualizer(
             self.builder, output_port=self.station.GetOutputPort("geometry_query"), zmq_url=zmq_url)
         self.diagram = self.builder.Build()
         self.simulator = Simulator(self.diagram)
+        self.simulator.set_target_realtime_rate(1.0)
+
         self.context = self.simulator.get_context()
         self.station_context = self.station.GetMyContextFromRoot(self.context)
+        self.plant = juggler_station.get_multibody_plant()
 
         self.controller = None #TODO: implement LeafSystem-based high level controller
-        
-    def command_position(self, iiwa_position, visualize=True, duration=1.0, final=True):
+
+
+    def command_iiwa_position(self, iiwa_position, simulate=True, duration=0.1, final=True, verbose=False):
         """
         Command the arm to move to a specific configuration
 
         Args:
             iiwa_position (list): the (length 7 for 7 DoF) list indicating arm joint positions
-            visualize (bool, optional): whether or not to visualize the command. Defaults to True.
-            duration (float, optional): duration to complete command in simulation. Defaults to 1.0.
+            simulate (bool, optional): whether or not to visualize the command. Defaults to True.
+            duration (float, optional): duration to complete command in simulation. Defaults to 0.1.
             final (bool, optional): whether or not this is the final command in the sequence; relevant for recording. Defaults to True.
+            verbose (bool, optional): whether or not to print measured position change. Defaults to False.
         """        
         self.station.GetInputPort("iiwa_position").FixValue(
             self.station_context, iiwa_position)
+    
         
-        if visualize:
+        if simulate:
             self.visualizer.start_recording()
             self.simulator.AdvanceTo(self.time + duration)
             self.visualizer.stop_recording()
+            
+            self.log.append(self.station.GetOutputPort("iiwa_position_measured").Eval(self.station_context))
+            
+            # print(self.plant.EvalBodyPoseInWorld(self.context, ))
+            if verbose:
+                print("Commanding position: {}\nMeasured Position: {}\n\n".format(iiwa_position, self.station.GetOutputPort("iiwa_position_measured").Eval(self.station_context)))
             
             if final:
                 self.visualizer.publish_recording()
@@ -58,9 +82,26 @@ class Juggler:
         self.time += duration
 
 
+    def command_iiwa_pose(self, iiwa_pose):
+        return
+
+
 
 
 if __name__ == "__main__":
-    juggler = Juggler()
-    juggler.command_position([0, np.pi/2, 0, -np.pi/2, 0, -np.pi/4, 0], final=False)
-    juggler.command_position([0, 0, 0,0, 0, 0, 0])
+    kp = 100
+    ki = 1
+    kd = 20
+    time_step = .002
+
+    juggler = Juggler(
+        kp=kp, 
+        ki=ki, 
+        kd=kd, 
+        time_step=time_step)
+
+    positions = [[r, np.pi/4, 0, -np.pi/2, 0, -np.pi/4, 0 ] for r in np.linspace(0, np.pi, 20)]
+    for i, pos in enumerate(positions):
+        juggler.command_iiwa_position(pos, duration=0.1, final=i==len(positions)-1, verbose=True)
+
+    print(pd.DataFrame(juggler.log))
--- utils/station.py
+++ utils/station.py
@@ -19,109 +19,136 @@ from pydrake.all import (
 from manipulation.scenarios import AddIiwa, AddWsg, AddRgbdSensors
 from manipulation.utils import FindResource
 
-def make_manipulation_station(time_step=0.002):
-    builder = DiagramBuilder()
+class JugglerStation:
+    def __init__(self, kp=100, ki=1, kd=20, time_step=0.002):
+        self.kp = kp
+        self.ki = ki
+        self.kd = kd
+        self.time_step = time_step
+        self.diagram, self.plant = self.make_manipulation_station()
 
-    # Add (only) the iiwa, WSG, and cameras to the scene.
-    plant, scene_graph = AddMultibodyPlantSceneGraph(
-        builder, time_step=time_step)
-    iiwa = AddIiwa(plant)
-    wsg = AddWsg(plant, iiwa)
-    parser = Parser(plant)
-    parser.AddModelFromFile(
-        FindResource("models/camera_box.sdf"), "camera0")
-    parser.AddModelFromFile("utils/end_effectors/paddle.sdf")
-    plant.WeldFrames(plant.GetFrameByName("iiwa_link_7"), plant.GetFrameByName("base_link"))
-    plant.Finalize()
+    def get_multibody_plant(self):
+        return self.plant
+
+    def get_diagram(self):
+        return self.diagram
+
+    @staticmethod
+    def make_manipulation_station(kp=100, ki=1, kd=20, time_step=0.002):
+        """
+        Create the juggler manipulation station.
+
+        Args:
+            kp (int, optional): proportional gain. Defaults to 100.
+            ki (int, optional): integral gain. Defaults to 1.
+            kd (int, optional): derivative gain. Defaults to 20.
+            time_step (float, optional): controller time step. Defaults to 0.002.
+
+        Returns:
+            (tuple[(diagram), (plant)]): the diagram and plant
+        """        
+        builder = DiagramBuilder()
+
+        # Add (only) the iiwa, WSG, and cameras to the scene.
+        plant, scene_graph = AddMultibodyPlantSceneGraph(
+            builder, time_step=time_step)
+        iiwa = AddIiwa(plant)
+        wsg = AddWsg(plant, iiwa)
+        parser = Parser(plant)
+        parser.AddModelFromFile(
+            FindResource("models/camera_box.sdf"), "camera0")
+        parser.AddModelFromFile("utils/end_effectors/paddle.sdf")
+        plant.WeldFrames(plant.GetFrameByName("iiwa_link_7"), plant.GetFrameByName("base_link"))
+        plant.Finalize()
 
 
-    num_iiwa_positions = plant.num_positions(iiwa)
+        num_iiwa_positions = plant.num_positions(iiwa)
 
-    # I need a PassThrough system so that I can export the input port.
-    iiwa_position = builder.AddSystem(PassThrough(num_iiwa_positions))
-    builder.ExportInput(iiwa_position.get_input_port(), "iiwa_position")
-    builder.ExportOutput(iiwa_position.get_output_port(), "iiwa_position_command")
+        # I need a PassThrough system so that I can export the input port.
+        iiwa_position = builder.AddSystem(PassThrough(num_iiwa_positions))
+        builder.ExportInput(iiwa_position.get_input_port(), "iiwa_position")
+        builder.ExportOutput(iiwa_position.get_output_port(), "iiwa_position_command")
 
-    # Export the iiwa "state" outputs.
-    demux = builder.AddSystem(Demultiplexer(
-        2 * num_iiwa_positions, num_iiwa_positions))
-    builder.Connect(plant.get_state_output_port(iiwa), demux.get_input_port())
-    builder.ExportOutput(demux.get_output_port(0), "iiwa_position_measured")
-    builder.ExportOutput(demux.get_output_port(1), "iiwa_velocity_estimated")
-    builder.ExportOutput(plant.get_state_output_port(iiwa), "iiwa_state_estimated")
+        # Export the iiwa "state" outputs.
+        demux = builder.AddSystem(Demultiplexer(
+            2 * num_iiwa_positions, num_iiwa_positions))
+        builder.Connect(plant.get_state_output_port(iiwa), demux.get_input_port())
+        builder.ExportOutput(demux.get_output_port(0), "iiwa_position_measured")
+        builder.ExportOutput(demux.get_output_port(1), "iiwa_velocity_estimated")
+        builder.ExportOutput(plant.get_state_output_port(iiwa), "iiwa_state_estimated")
 
-    # Make the plant for the iiwa controller to use.
-    controller_plant = MultibodyPlant(time_step=time_step)
-    controller_iiwa = AddIiwa(controller_plant)
-    AddWsg(controller_plant, controller_iiwa, welded=True)
-    controller_plant.Finalize()
+        # Make the plant for the iiwa controller to use.
+        controller_plant = MultibodyPlant(time_step=time_step)
+        controller_iiwa = AddIiwa(controller_plant)
+        AddWsg(controller_plant, controller_iiwa, welded=True)
+        controller_plant.Finalize()
 
-    # Add the iiwa controller
-    iiwa_controller = builder.AddSystem(
-        InverseDynamicsController(
-            controller_plant,
-            kp=[100]*num_iiwa_positions,
-            ki=[1]*num_iiwa_positions,
-            kd=[20]*num_iiwa_positions,
-            has_reference_acceleration=False))
-    iiwa_controller.set_name("iiwa_controller")
-    builder.Connect(
-        plant.get_state_output_port(iiwa), iiwa_controller.get_input_port_estimated_state())
+        # Add the iiwa controller
+        iiwa_controller = builder.AddSystem(
+            InverseDynamicsController(
+                controller_plant,
+                kp=[kp]*num_iiwa_positions,
+                ki=[ki]*num_iiwa_positions,
+                kd=[kd]*num_iiwa_positions,
+                has_reference_acceleration=False))
+        iiwa_controller.set_name("iiwa_controller")
+        builder.Connect(
+            plant.get_state_output_port(iiwa), iiwa_controller.get_input_port_estimated_state())
 
-    # Add in the feed-forward torque
-    adder = builder.AddSystem(Adder(2, num_iiwa_positions))
-    builder.Connect(iiwa_controller.get_output_port_control(),
-                    adder.get_input_port(0))
-    # Use a PassThrough to make the port optional (it will provide zero values if not connected).
-    torque_passthrough = builder.AddSystem(
-        PassThrough([0]*num_iiwa_positions))
-    builder.Connect(torque_passthrough.get_output_port(), adder.get_input_port(1))
-    builder.ExportInput(torque_passthrough.get_input_port(), "iiwa_feedforward_torque")
-    builder.Connect(adder.get_output_port(), plant.get_actuation_input_port(iiwa))
+        # Add in the feed-forward torque
+        adder = builder.AddSystem(Adder(2, num_iiwa_positions))
+        builder.Connect(iiwa_controller.get_output_port_control(),
+                        adder.get_input_port(0))
+        # Use a PassThrough to make the port optional (it will provide zero values if not connected).
+        torque_passthrough = builder.AddSystem(
+            PassThrough([0]*num_iiwa_positions))
+        builder.Connect(torque_passthrough.get_output_port(), adder.get_input_port(1))
+        builder.ExportInput(torque_passthrough.get_input_port(), "iiwa_feedforward_torque")
+        builder.Connect(adder.get_output_port(), plant.get_actuation_input_port(iiwa))
 
-    # Add discrete derivative to command velocities.
-    desired_state_from_position = builder.AddSystem(
-        StateInterpolatorWithDiscreteDerivative(
-            num_iiwa_positions, time_step, suppress_initial_transient=True))
-    desired_state_from_position.set_name("desired_state_from_position")
-    builder.Connect(desired_state_from_position.get_output_port(),      
-                    iiwa_controller.get_input_port_desired_state())
-    builder.Connect(iiwa_position.get_output_port(), desired_state_from_position.get_input_port())
+        # Add discrete derivative to command velocities.
+        desired_state_from_position = builder.AddSystem(
+            StateInterpolatorWithDiscreteDerivative(
+                num_iiwa_positions, time_step, suppress_initial_transient=True))
+        desired_state_from_position.set_name("desired_state_from_position")
+        builder.Connect(desired_state_from_position.get_output_port(),      
+                        iiwa_controller.get_input_port_desired_state())
+        builder.Connect(iiwa_position.get_output_port(), desired_state_from_position.get_input_port())
 
-    # Export commanded torques.
-    #builder.ExportOutput(adder.get_output_port(), "iiwa_torque_commanded")
-    #builder.ExportOutput(adder.get_output_port(), "iiwa_torque_measured")
+        # Export commanded torques.
+        #builder.ExportOutput(adder.get_output_port(), "iiwa_torque_commanded")
+        #builder.ExportOutput(adder.get_output_port(), "iiwa_torque_measured")
 
-    # Wsg controller.
-    wsg_controller = builder.AddSystem(SchunkWsgPositionController())
-    wsg_controller.set_name("wsg_controller")
-    builder.Connect(wsg_controller.get_generalized_force_output_port(),             
-                    plant.get_actuation_input_port(wsg))
-    builder.Connect(plant.get_state_output_port(wsg), wsg_controller.get_state_input_port())
-    builder.ExportInput(wsg_controller.get_desired_position_input_port(), "wsg_position")
-    builder.ExportInput(wsg_controller.get_force_limit_input_port(), "wsg_force_limit")
-    wsg_mbp_state_to_wsg_state = builder.AddSystem(
-        MakeMultibodyStateToWsgStateSystem())
-    builder.Connect(plant.get_state_output_port(wsg), wsg_mbp_state_to_wsg_state.get_input_port())
-    builder.ExportOutput(wsg_mbp_state_to_wsg_state.get_output_port(), "wsg_state_measured")
-    builder.ExportOutput(wsg_controller.get_grip_force_output_port(), "wsg_force_measured")
+        # Wsg controller.
+        wsg_controller = builder.AddSystem(SchunkWsgPositionController())
+        wsg_controller.set_name("wsg_controller")
+        builder.Connect(wsg_controller.get_generalized_force_output_port(),             
+                        plant.get_actuation_input_port(wsg))
+        builder.Connect(plant.get_state_output_port(wsg), wsg_controller.get_state_input_port())
+        builder.ExportInput(wsg_controller.get_desired_position_input_port(), "wsg_position")
+        builder.ExportInput(wsg_controller.get_force_limit_input_port(), "wsg_force_limit")
+        wsg_mbp_state_to_wsg_state = builder.AddSystem(
+            MakeMultibodyStateToWsgStateSystem())
+        builder.Connect(plant.get_state_output_port(wsg), wsg_mbp_state_to_wsg_state.get_input_port())
+        builder.ExportOutput(wsg_mbp_state_to_wsg_state.get_output_port(), "wsg_state_measured")
+        builder.ExportOutput(wsg_controller.get_grip_force_output_port(), "wsg_force_measured")
 
-    # Cameras.
-    AddRgbdSensors(builder, plant, scene_graph)
+        # Cameras.
+        AddRgbdSensors(builder, plant, scene_graph)
 
-    # Export "cheat" ports.
-    builder.ExportOutput(scene_graph.get_query_output_port(), "geometry_query")
-    builder.ExportOutput(plant.get_contact_results_output_port(), "contact_results")
-    builder.ExportOutput(plant.get_state_output_port(), "plant_continuous_state")
+        # Export "cheat" ports.
+        builder.ExportOutput(scene_graph.get_query_output_port(), "geometry_query")
+        builder.ExportOutput(plant.get_contact_results_output_port(), "contact_results")
+        builder.ExportOutput(plant.get_state_output_port(), "plant_continuous_state")
 
-    diagram = builder.Build()
+        diagram = builder.Build()
 
-    return diagram
+        return diagram, plant
 
 
 def station_test():
     builder = DiagramBuilder()
-    station = builder.AddSystem(make_manipulation_station())
+    station = builder.AddSystem(JugglerStation().get_diagram())
 
     visualizer = ConnectMeshcatVisualizer(
         builder, output_port=station.GetOutputPort("geometry_query"), zmq_url=zmq_url)
