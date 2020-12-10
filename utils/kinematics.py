adding v_z mirroring taper, fixing pitch sign

utils/kinematics.py
@@ -5,6 +5,8 @@ from pydrake.all import (
     ConstantVectorSource, RigidTransform, SpatialVelocity, RollPitchYaw
 )
 
+CENTERPOINT = np.array([0.88, 0])
+
 class InverseKinematics(LeafSystem):
     def __init__(self, plant):
         LeafSystem.__init__(self)
@@ -59,7 +61,8 @@ class VelocityMirror(LeafSystem):
     def CalcOutput(self, context, output):
         q = self.GetInputPort("iiwa_pos_measured").Eval(context)
         v = self.GetInputPort("iiwa_velocity_estimated").Eval(context)
-        p_Ball_xy = np.array(self.GetInputPort("ball_pose").Eval(context))[:2]
+        p_Ball = np.array(self.GetInputPort("ball_pose").Eval(context))
+        p_Ball_xy, p_Ball_z = p_Ball[:2], np.array([p_Ball[2]])
         v_Ball = np.array(self.GetInputPort("ball_velocity").Eval(context))
         v_Ball_xy, v_Ball_z = v_Ball[:2], np.array([v_Ball[2]])
 
@@ -68,10 +71,20 @@ class VelocityMirror(LeafSystem):
             output.SetFromVector([0, 0, 0])
             return
 
+
+        # if v_Ball_z >= 0:
+        #     v_Ball_z *= -1
+        # else:
+        #     v_Ball_z *= -1
+
+        # This might help prevent it from hitting the ball harder and harder
+        # by tapering off the mirror velocity as the ball gets faster, but maintaining
+        # approx linear match at lower speeds
         if v_Ball_z >= 0:
-            v_Ball_z *= -1
+            v_Ball_z = -2*np.log(v_Ball_z+1)
         else:
-            v_Ball_z *= -1   
+            v_Ball_z = 2*np.log(-v_Ball_z+1)
+
         self.plant.SetPositionsAndVelocities(self.plant_context, self.iiwa, np.hstack([q, v]))
         p_Paddle_xy = np.array(self.plant.EvalBodyPoseInWorld(self.plant_context, self.Paddle).translation())[:2]
         v_Paddle_xy = np.array(self.plant.EvalBodySpatialVelocityInWorld(self.plant_context, self.Paddle).translational())[:2]
@@ -80,8 +93,11 @@ class VelocityMirror(LeafSystem):
         K_d = 1
         v_P_desired = K_p*(p_Ball_xy - p_Paddle_xy) + K_d*(v_Ball_xy - v_Paddle_xy)
         v_P_desired = np.concatenate((v_P_desired, v_Ball_z))
+        
         # Tune down with radial shape (1-x^2 - y^2)
-        scale = (1 - (p_Ball_xy[0] - .88)**2 - p_Ball_xy[1]**2)
+        scale = (1 - 1.15*(p_Ball_xy[0] - CENTERPOINT[0])**2 - p_Ball_xy[1]**2)
+
+        # IDEA: Scale this further with the height of the ball?
         v_P_desired[2] *= scale
         output.SetFromVector(v_P_desired)
 
@@ -115,12 +131,11 @@ class AngularVelocityTilt(LeafSystem):
             output.SetFromVector([0, 0, 0])
             return
         
-        k = np.array([1, 4])
-        centerpoint = np.array([0.9, 0])
+        k = np.array([4, 4])
         # roll_des = np.sign(B_y) * np.arctan(k * (1 - np.cos(B_y)))
         # pitch_des = np.sign(B_x - centerpoint) * np.arctan(k * (1 - np.cos(B_x - centerpoint_x)))
-        deltas = np.sign(X_B[:2]) * np.arctan(k * (1 - np.cos(X_B[:2] - centerpoint)))
-        pitch_des = deltas[0]
+        deltas = np.sign(X_B[:2] - CENTERPOINT) * np.array([.9, .9]) * np.arctan(k * (1 - np.cos(X_B[:2] - CENTERPOINT)))
+        pitch_des = -deltas[0]
         roll_des = deltas[1]
         # yaw_des = np.arctan(centerpoint[1]/centerpoint[0])
         # roll_des = np.arctan(.75*(B_y)**3)